7ceddfd1cacfe8f9a6898d57c955043d6b1d5cf9
BoFo <brunofazio466@gmail.com>
2025-02-13 19:41:07 -0300

    dia 2 wins

---
+++ practica2025-2.py
@@ -0,0 +1,18 @@
+# practicando listas paralelas, sintaxis y algoritmos basicos
+# ejercicio finalPrevio(parecido):
+"""Crear un programa para votar series 
+    - primero cargar los codigos de las series sin duplicados 
+    - consultar por condigo de serie la puntuacion y almacenarlo
+    - imprimir en consola el resultado como una tabla 
+    - devolver la mejor calificada
+"""
+
+def main():
+    codSeries = []
+    puntuacion = []
+
+    cargaInicial()
+
+#1 ejecuicion del main
+if __name__ == '__main__':
+    main()
