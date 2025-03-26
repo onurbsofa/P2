84c4d2bc3476f19ecdbbe61da080b78b0743dc66
BoFo <brunofazio466@gmail.com>
2025-03-26 16:18:28 -0300

    tupla ejemplo

---
+++ FinalPrevioProgra1/Dia3.py
@@ -0,0 +1,15 @@
+# tuplas 
+# simulacion turno de batalla naval
+
+turno1 = ()
+
+def jugarTurno(turno):
+
+    x = int(input("ingrese una cordenada x"))
+    y = int(input("ingrese una cordenada x"))
+        
+    turno = (x,y)
+    print(turno)
+    print(type(turno))
+
+jugarTurno(turno1)
